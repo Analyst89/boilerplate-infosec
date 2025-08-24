300e5d3a81b82ee6f4f683384ee928deaffa6c53
Analyst89 <degen.andrewc@gmail.com>
2025-08-24 17:09:41 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet.hidePoweredBy());
+app.use(helmet.noSniff());
 app.use(helmet.frameguard({ action: 'deny' }));
 app.use(helmet.xssFilter());
-app.use(helmet.noSniff());
+
 
 
 
