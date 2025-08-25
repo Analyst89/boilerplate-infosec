73cc6a90947d68f822ec5593205ed877cad8b86c
Analyst89 <degen.andrewc@gmail.com>
2025-08-24 21:58:24 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -11,7 +11,7 @@ app.use(helmet.xssFilter());
 app.use(helmet.ieNoOpen());
 app.use(helmet.hsts({maxAge:ninetyDaysInSeconds = 90*24*60*60, force: true}));
 app.use(helmet.dnsPrefetchControl());
-
+app.use(helmet.noCache());
 
 
 
